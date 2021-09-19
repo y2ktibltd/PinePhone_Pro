cdddf448e97666d27933bbe4946d00977643a086
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2021-09-19 10:08:38 -0400

    Update setup.sh

--- Arch/setup.sh
+++ Arch/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 useradd -mG wheel $1
 passwd $1
-pacman -Syu htop tmux base-devel  git curl  xorg xterm cmake nano vim
+pacman -Syu htop tmux base-devel  git curl  xorg xterm cmake nano vim python-setuptools
 
 cd /opt
 git clone https://aur.archlinux.org/yay-git.git
@@ -11,4 +11,12 @@ chown -R $1:$1 ./yay-git
 cd yay-git
 makepkg -si
 
-yay -S qtile python-setuptools
+yay -S qtile 
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#launch vim
+#PlugInstall
+#exit vim
+.vim/plugged/YouCompleteMe/install.py
