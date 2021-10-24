68f180ccf15750af207f77af9687e8912329a22a
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2021-10-24 17:17:59 -0400

    Update setup.sh

--- Arch/setup.sh
+++ Arch/setup.sh
@@ -3,22 +3,15 @@ echo "Please enter your new username"
 read user
 useradd -mG wheel $user
 echo -e $user:$user | chpasswd
-
 pacman -Syu --noconfirm tmux gdb clang curl vim nano htop base-devel git curl cmake python-setuptools xorg xterm
-
 git clone https://aur.archlinux.org/yay-git.git /opt/yay-git
 chown -R $user:$user /opt/yay-git
-
 cd /opt/yay-git
 echo "y" | sudo -S -u $user makepkg -si
-
 sudo -S -u $user yay -S --noconfirm qtile
-
 sudo -S -u $user echo "alias ll='ls -lah --color=auto'">>/home/$user/.bashrc
-
 sudo -S -u $user curl -fLo /home/$user/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo -S -u $user echo """syntax on
 
 set nu
@@ -46,6 +39,5 @@ Plug 'Valloric/YouCompleteMe'
 Plug 'mbbill/undotree'
 Plug 'vim-utils/vim-man'
 call plug#end()""" > /home/$user/.vimrc
-
-sudo -S -u $user vim -c :PlugInstall +qall +qall
-echo $user | su $user /home/$user/.vim/plugged/YouCompleteMe/install.py
+sudo -S -u $user vim -c :PlugInstall +qall
+pkexec --user $user /home/$user/.vim/plugged/YouCompleteMe/install.py
